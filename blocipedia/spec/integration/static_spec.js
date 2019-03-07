7df3141302158bec830086578b53914aa218bbc9
Raissa Bola <raissabola@gmail.com>
2019-03-06 22:11:05 -0800

    Checkpoint Scaffolding: Completed

--- blocipedia/spec/integration/static_spec.js
+++ blocipedia/spec/integration/static_spec.js
@@ -6,11 +6,11 @@ describe("routes : static", () => {
 
   describe("GET /", () => {
 
-    it("should return status code 200", (done) => {
+    it("should return status code 200 have 'Welcome to Blocipedia' in the body of response", (done) => {
 
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
+        expect(body).toContain("Welcome to Blocipedia");
         done();
       });
     });
