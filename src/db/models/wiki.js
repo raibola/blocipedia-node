bc3d649ffef0d3fa0574b5e6d9fde9aef366854a
Raissa Bola <raissabola@gmail.com>
2019-03-13 18:27:29 -0700

    Create not working

---
+++ src/db/models/wiki.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Wiki = sequelize.define('Wiki', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    private: {
+      type: DataTypes.BOOLEAN
+    },
+    userId: {
+      type: DataTypes.INTEGER
+    }
+  }, {});
+  Wiki.associate = function(models) {
+    Wiki.belongsTo(models.User, {
+      foreignKey: "userId",
+      onDelete: "CASCADE",
+    });
+  };
+  return Wiki;
+};
---
+++ src/routes/wikis.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const wikiController = require("../controllers/wikiController")
+
+router.get("/wikis", wikiController.index);
+router.get("/wikis/new", wikiController.new);
+router.post("/wikis/create", wikiController.create);
+router.get("/wikis/:id", wikiController.show);
+router.post("/wikis/:id/destroy", wikiController.destroy);
+
+module.exports = router;
